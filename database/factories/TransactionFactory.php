778811ce3682578d392f609cf64747028e1c5b61
Ronaldo Trejo <62269427+maronte@users.noreply.github.com>
2020-10-23 01:49:32 -0500

    Endpoints for category, product and transaction

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -2,18 +2,17 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Buyer;
+use App\User;
 use App\Seller;
 use App\Transaction;
 use Faker\Generator as Faker;
 
 $factory->define(Transaction::class, function (Faker $faker) {
-
     $vendedor = Seller::has('products')->get()->random();
-    $comprador = Buyer::all()->except($vendedor->id)->random();
+    $comprador = User::all()->except($vendedor->id)->random();
 
     return [
-        'quantity' => $faker->numberBetween(1, 4),
+        'quantity' => $faker->numberBetween(1, 3),
         'buyer_id' => $comprador->id,
         'product_id' => $vendedor->products->random()->id,
     ];
