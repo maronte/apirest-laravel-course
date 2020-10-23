05dbcc2f31b42d98cb222b2f9acced8917a8859a
Ronaldo Trejo <62269427+maronte@users.noreply.github.com>
2020-10-23 15:32:41 -0500

    Complex endpoints for seller model

---
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Http\Controllers\ApiController;
+use App\Product;
+use App\Seller;
+use App\User;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class SellerProductController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Seller $seller)
+    {
+        $products = $seller->products;
+
+        return $this->showAll($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, User $seller)
+    {
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|integer|min:1',
+            //'image' => 'required|image'
+        ];
+
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+
+        $data['status'] = Product::PRODUCTO_NO_DISPONIBLE;
+        // Temporal line
+        $data['image'] = '1.jpg';
+        $data['seller_id'] = $seller->id;
+
+        $product = Product::create($data);
+
+        return $this->showOne($product, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Seller $seller, Product $product)
+    {
+        $rules = [
+            'quantity' => 'integer|min:1',
+            'status' => 'in:' . Product::PRODUCTO_DISPONIBLE . ',' . Product::PRODUCTO_NO_DISPONIBLE,
+            //'image' => 'image'
+        ];
+
+        $this->validate($request, $rules);
+
+        $this->verificarVendedor($seller, $product);
+
+        $product->fill($request->only(['name', 'description', 'quantity']));
+
+        if($request->has('status')) {
+
+            $product->status = $request->status;
+
+            if($product->estaDisponible() && $product->categories()->count() == 0){
+                return $this->errorResponse('Un producto activo debe tener al menos una categoria', 409);
+            }
+        }
+
+        if ($product->isClean()){
+            return $this->errorResponse('Debe haber por lo menos un valor diferente para actualizar', 422);
+        }
+
+        $product->save();
+
+        return $this->showOne($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Seller $seller, Product $product)
+    {
+        $this->verificarVendedor($seller, $product);
+
+        $product->delete();
+
+        return $this->showOne($product);
+    }
+
+    protected function verificarVendedor(Seller $seller, Product $product){
+        if ($product->seller_id != $seller->id){
+            throw new HttpException('El vendedor especificado no es el dueño del producto', 422);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,20 @@ Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['in
  */
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index','show']]);
 
+Route::resource('sellers.transactions', 'Seller\SellerTransactionController', 
+ ['only' => ['index']]);
+
+Route::resource('sellers.buyers', 'Seller\SellerBuyerController', 
+ ['only' => ['index']]);
+
+Route::resource('sellers.categories', 'Seller\SellerCategoryController', 
+ ['only' => ['index']]);
+
+ Route::resource('sellers.products', 'Seller\SellerProductController', 
+ ['except' => ['show']]);
+
+
+
 /**
  * Users
  */
